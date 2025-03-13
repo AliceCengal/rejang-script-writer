5a37ca7bf829fa8dc6f752181b3c7721c5578cb2
Athran Rahman <cengallut@gmail.com>
2025-03-13 15:46:08 +0800

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/design-system/",
+  base: "/rejang-script-writer/",
   plugins: [react()],
 });
